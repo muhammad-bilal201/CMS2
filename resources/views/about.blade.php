f3d3312e135070c9c3f1ff07cde2e3dcba1c2dfa
muhammad-bilal201 <muhamamd.bilal.amanat004@gmail.com>
2022-03-25 21:57:37 +0500

    Mess

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,7 +11,7 @@
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="app/vender/css/styles.css" rel="stylesheet" />
+    <link href="{{asset('css/styles.css')}}" rel="stylesheet" />
 </head>
 <body>
 <header class="bg-dark py-5">
@@ -35,6 +35,6 @@
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Core theme JS-->
-<script src="/app/vender/js/scripts.js"></script>
+<script src="{{asset('js/scripts.js')}}"></script>
 </body>
 </html>
